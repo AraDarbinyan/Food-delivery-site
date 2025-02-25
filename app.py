4cdc7a852a9fb889853f8a10d62d4eba0bd0c691
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2025-02-25 20:12:14 +0400

    created cart page and function wich adds products into user's cart and photo shop-cart which is a link into a cart

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import Product, Customer, db
+from models import Product, Customer, Cart, CartProduct, db
 
 app = Flask(__name__)
 
@@ -34,6 +34,40 @@ def category(category_name):
     return render_template('category.html', products=products)
 
 
+@app.route('/cart/')
+@login_required
+def cart():
+    cart = Cart.query.filter_by(customer_id=current_user.id).first()
+    if not cart:
+        flash('Your cart is empty.')
+        return redirect(url_for('menu'))
+    
+    cart_products = CartProduct.query.filter_by(cart_id=cart.id).all()
+    return render_template('cart.html',  cart_products=cart_products)
+
+
+@app.route('/add_to_cart/<int:product_id>', methods=['POST'])
+def add_to_cart(product_id):
+    product = Product.query.get_or_404(product_id)
+    cart = Cart.query.filter_by(customer_id=current_user.id).first()
+    if not cart:
+        cart = Cart(customer_id=current_user.id)
+        db.session.add(cart)
+        db.session.commit()
+    
+    cart_product = CartProduct.query.filter_by(cart_id=cart.id, product_id=product.id).first()
+    if not cart_product:
+        new_cart_product = CartProduct(cart_id=cart.id, product_id=product.id)
+        db.session.add(new_cart_product)
+    else:
+        cart_product.quantity += 1
+        
+    db.session.commit()
+
+    flash(f'{product.name} added to cart successfully!')
+    return redirect(url_for('menu'))
+
+
 @login_meneger.user_loader
 def load_user(user_id):
     return Customer.query.get(int(user_id))
@@ -41,6 +75,9 @@ def load_user(user_id):
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
